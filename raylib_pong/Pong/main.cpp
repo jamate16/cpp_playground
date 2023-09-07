c69da75e2f00e84b5b8e0afaca657af2f70f7253
jamate <mateoarcilalopez@gmail.com>
2023-09-07 13:57:13 -0500

    refactor collision handling and position/speed update logic

--- raylib_pong/Pong/main.cpp
+++ raylib_pong/Pong/main.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
-
 /*
-	This code is based on Jones tutorial on YouTube: https://www.youtube.com/watch?v=LvpS3ILwQNA
+	This code is based on Jones' tutorial on YouTube: https://www.youtube.com/watch?v=LvpS3ILwQNA
 */
+#include <iostream>
 
 namespace rl {
 	#include "raylib.h"
@@ -17,14 +16,62 @@ struct PlayableRectangle {
 	}
 };
 
+
+struct Paddle {
+	enum PaddleType {
+		LEFT = 0,
+		RIGHT
+	};
+
+	float width, height;
+	PaddleType type;
+
+	// Position of top left corner
+	rl::Vector2 position;
+	float speedY;
+
+	Paddle(int width, int height, PaddleType type, float speed = 1)
+		: width{ static_cast<float>(width) }, height{ static_cast<float>(height) }, position{ 0.0, 0.0 }, type{ type }, speedY{ speed } {
+
+	}
+	// Another way we can do this and the ball too, is having a "positioner" class that will take care of the positioning of the objects
+	void positionSidePlayableArea(const PlayableRectangle& rect) {
+		switch (type) {
+		case PaddleType::RIGHT:
+			position.x = rect.origin.x + rect.dimentions.x - 0.0625 * rect.dimentions.x - width;
+			break;
+		case PaddleType::LEFT:
+		default:
+			position.x = rect.origin.x + 0.0625 * (rect.dimentions.x);
+		}
+		position.y = rect.origin.y + rect.dimentions.y / 2 - height / 2;
+	}
+
+	void keepInsidePlayableArea(const PlayableRectangle& rect) {
+		// Check yAxis only
+		if (position.y < rect.origin.y) {
+			position.y = rect.origin.y;
+			//speedY *= -1; // Leaving this creates a funny behavior, try it out. It's not a bug it's a feature
+		}
+		if (position.y > rect.origin.y + rect.dimentions.y - height) {
+			position.y = rect.origin.y + rect.dimentions.y - height;
+			//speedY *= - 1; // Leaving this creates a funny behavior, try it out. It's not a bug it's a feature
+		}
+	}
+
+	void updatePosition(float deltaTime) {
+		position.y += speedY * deltaTime;
+	}
+};
+
 struct Ball {
 	double radius;
 
 	rl::Vector2 position;
 	rl::Vector2 speed;
-	
 
-	Ball(int radiusInPixels, float speedX=0.0, float speedY=0.0)
+
+	Ball(int radiusInPixels, float speedX = 0.0, float speedY = 0.0)
 		: radius{ static_cast<float>(radiusInPixels) } {
 		position = { 0.0, 0.0 };
 		speed = { speedX, speedY };
@@ -54,47 +101,28 @@ struct Ball {
 			speed.y *= -1;
 		}
 	}
-};
 
-struct Paddle {
-	enum PaddleType {
-		LEFT = 0,
-		RIGHT
-	};
-
-	float width, height;
-
-	// Position of top left corner
-	rl::Vector2 position;
-	float speedY;
-
-	Paddle(int width, int height, float speed=1)
-		: width{ static_cast<float>(width) }, height{ static_cast<float>(height) }, position{ 0.0, 0.0 }, speedY{ speed } {
-
-	}
-	// Another way we can do this and the ball too, is having a "positioner" class that will take care of the positioning of the objects
-	void positionSidePlayableArea(const PlayableRectangle& rect, const PaddleType& paddleType) {
-		switch (paddleType) {
-		case PaddleType::RIGHT:
-			position.x = rect.origin.x + rect.dimentions.x - 0.0625 * rect.dimentions.x - width;
-			break;
-		case PaddleType::LEFT:
-		default:
-			position.x = rect.origin.x + 0.0625 * (rect.dimentions.x);
-		}
-		position.y = rect.origin.y + rect.dimentions.y / 2 - height / 2;
+	bool checkCollisionWithPaddle(const Paddle& paddle) {
+		return rl::CheckCollisionCircleRec(rl::Vector2{ position.x, position.y }, radius,
+			rl::Rectangle{ paddle.position.x,
+						   paddle.position.y,
+						   paddle.width,
+						   paddle.height });
 	}
 
-	void keepInsidePlayableArea(const PlayableRectangle& rect) {
-		// Check yAxis only
-		if (position.y < rect.origin.y) {
-			position.y = rect.origin.y;
-			//speedY *= -1; // Leaving this creates a funny behavior
-		}
-		if (position.y > rect.origin.y + rect.dimentions.y - height) {
-			position.y = rect.origin.y + rect.dimentions.y - height;
-			//speedY *= - 1; // Leaving this creates a funny behavior
-		}
+	void handleCollisionWithPaddle(const Paddle& paddle) {
+		if (paddle.type == Paddle::LEFT) position.x = paddle.position.x + paddle.width + radius;
+		else position.x = paddle.position.x - radius;
+
+		speed.x *= -1;
+		speed.y = 500 * (position.y - (paddle.position.y + paddle.height / 2)) / paddle.height / 2;
+	}
+
+	void updatePosition(float deltaTime) {
+		position.x += speed.x * deltaTime;
+		position.y += speed.y * deltaTime;
+
+		// Here I could do bound checking
 	}
 };
 
@@ -104,62 +132,40 @@ int main() {
 
 	rl::InitWindow(gameArea.dimentions.x, gameArea.dimentions.y, "Pong"); // First create an application window
 	rl::SetWindowState(rl::FLAG_VSYNC_HINT); // Turn V-sync on
-	
+
 	// Initialize components
-	Ball ball{5, -500};
-	Paddle leftPaddle{10, 100, 500};
-	Paddle rightPaddle{10, 100, 500};
+	Ball ball{ 5, -500 };
+	Paddle leftPaddle{ 10, 100, Paddle::LEFT, 500 };
+	Paddle rightPaddle{ 10, 100, Paddle::RIGHT, 500 };
 
 	ball.positionMiddlePlayableArea(gameArea);
-	leftPaddle.positionSidePlayableArea(gameArea, Paddle::LEFT);
-	rightPaddle.positionSidePlayableArea(gameArea, Paddle::RIGHT);
+	leftPaddle.positionSidePlayableArea(gameArea);
+	rightPaddle.positionSidePlayableArea(gameArea);
 
 	const char* winnerText = nullptr;
 
 	while (!rl::WindowShouldClose()) {
-		// Update
+		// Update paddle speeds
+		if (rl::IsKeyDown(rl::KEY_W)) leftPaddle.speedY = (leftPaddle.speedY > 0) ? -leftPaddle.speedY : leftPaddle.speedY;
+		if (rl::IsKeyDown(rl::KEY_S)) leftPaddle.speedY = (leftPaddle.speedY > 0) ? leftPaddle.speedY : -leftPaddle.speedY;
+		if (rl::IsKeyDown(rl::KEY_UP)) rightPaddle.speedY = (rightPaddle.speedY > 0) ? -rightPaddle.speedY : rightPaddle.speedY;
+		if (rl::IsKeyDown(rl::KEY_DOWN)) rightPaddle.speedY = (rightPaddle.speedY > 0) ? rightPaddle.speedY : -rightPaddle.speedY;
+
+		// Update positions
+		ball.updatePosition(rl::GetFrameTime());
+		if (rl::IsKeyDown(rl::KEY_W) || rl::IsKeyDown(rl::KEY_S)) leftPaddle.updatePosition(rl::GetFrameTime());
+		if (rl::IsKeyDown(rl::KEY_UP) || rl::IsKeyDown(rl::KEY_DOWN)) rightPaddle.updatePosition(rl::GetFrameTime());
+
+		// Handle ball-paddle collisions if any
+		if (ball.checkCollisionWithPaddle(leftPaddle)) ball.handleCollisionWithPaddle(leftPaddle);
+		if (ball.checkCollisionWithPaddle(rightPaddle)) ball.handleCollisionWithPaddle(rightPaddle);
+
+		// Constraint game objects to playable area
 		ball.keepInsidePlayableArea(gameArea);
 		leftPaddle.keepInsidePlayableArea(gameArea);
 		rightPaddle.keepInsidePlayableArea(gameArea);
 
-		ball.position.x += ball.speed.x * rl::GetFrameTime();
-		ball.position.y += ball.speed.y * rl::GetFrameTime();
-
-		if (rl::IsKeyDown(rl::KEY_W)) {
-			leftPaddle.position.y -= leftPaddle.speedY * rl::GetFrameTime();
-		}
-		if (rl::IsKeyDown(rl::KEY_S)) {
-			leftPaddle.position.y += leftPaddle.speedY * rl::GetFrameTime();
-		}
-		if (rl::IsKeyDown(rl::KEY_UP)) {
-			rightPaddle.position.y -= rightPaddle.speedY * rl::GetFrameTime();
-		}
-		if (rl::IsKeyDown(rl::KEY_DOWN)) {
-			rightPaddle.position.y += rightPaddle.speedY * rl::GetFrameTime();
-		}
-
-		if (rl::CheckCollisionCircleRec(rl::Vector2{ ball.position.x, ball.position.y }, ball.radius,
-										rl::Rectangle{ leftPaddle.position.x,
-													   leftPaddle.position.y,
-													   leftPaddle.width,
-													   leftPaddle.height })) {
-			ball.position.x = leftPaddle.position.x + leftPaddle.width + ball.radius;
-			ball.speed.x *= -1;
-
-			ball.speed.y = 500 * (ball.position.y - (leftPaddle.position.y + leftPaddle.height/2)) / leftPaddle.height / 2;
-		}
-		if (rl::CheckCollisionCircleRec(rl::Vector2{ ball.position.x, ball.position.y }, ball.radius,
-										rl::Rectangle{ rightPaddle.position.x,
-													   rightPaddle.position.y,
-													   rightPaddle.width,
-													   rightPaddle.height })) {
-			ball.position.x = rightPaddle.position.x - ball.radius;
-			ball.speed.x *= -1;
-
-			ball.speed.y = 500 * (ball.position.y - (rightPaddle.position.y + rightPaddle.height / 2)) / rightPaddle.height / 2;
-		}
-
-
+		// Draw on screen
 		rl::BeginDrawing();
 
 			rl::ClearBackground(rl::RAYWHITE);
